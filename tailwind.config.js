d7388051563e33f9a7f346728fd0115c642c32ff
roganoalien <hola@rodrigogarcia.com.mx>
2020-12-30 04:01:42 -0600

    :fire: Hot fix
    Made a mistake with the purge config of the layout... was looking for a ./layout folder while it's named ./Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	purge: ['./pages/**/*.js', './components/**/*.js', './layout/**/*.js'],
+	purge: ['./pages/**/*.js', './components/**/*.js', './Layout/**/*.js'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		fontFamily: {
